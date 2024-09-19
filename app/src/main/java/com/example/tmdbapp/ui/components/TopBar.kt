b6348543619534472457d02dfd088942c3484094
androiddevnotes <awesomedevnotes@gmail.com>
2024-09-19 04:05:09 +0100

    Refactor TopBar by extracting distinct composable functions, disable "TooManyFunctions" rule in detekt configuration

--- app/src/main/java/com/example/tmdbapp/ui/components/TopBar.kt
+++ app/src/main/java/com/example/tmdbapp/ui/components/TopBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.layout.*
 import androidx.compose.ui.res.*
-import androidx.compose.ui.unit.*
 import com.example.tmdbapp.R
 import com.example.tmdbapp.ui.theme.*
 import com.example.tmdbapp.utils.*
@@ -41,149 +40,241 @@ fun TopBar(
       onCloseSearchClick = onCloseSearchClick,
     )
   } else {
-    TopAppBar(
-      title = {
-        Box(
-          modifier = Modifier.fillMaxWidth(),
-          contentAlignment = Alignment.Center,
-        ) {
-          Image(
-            painter = painterResource(id = R.drawable.cool_shape_arrow_right),
-            contentDescription = "App Logo",
-            modifier =
-              Modifier
-                .size(40.dp)
-                .align(Alignment.Center),
-          )
-        }
-      },
-      actions = {
-        IconButton(onClick = onSearchIconClick) {
-          Image(
-            painter = painterResource(id = R.drawable.cool_shape_search),
-            contentDescription = stringResource(R.string.content_desc_search),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-
-        Box {
-          IconButton(onClick = onDropdownExpand) {
-            Image(
-              painter = painterResource(id = R.drawable.cool_shape_sort),
-              contentDescription = stringResource(R.string.content_desc_sort),
-              contentScale = ContentScale.Fit,
-              modifier = Modifier.size(24.dp),
-            )
-          }
-          DropdownMenu(
-            expanded = expandedDropdown,
-            onDismissRequest = { onDropdownExpand() },
-            modifier = Modifier.background(MaterialTheme.colorScheme.surface),
-          ) {
-            SortOption.values().forEach { sortOption ->
-              DropdownMenuItem(
-                text = {
-                  Text(
-                    text = stringResource(sortOption.stringRes),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurface,
-                  )
-                },
-                onClick = { onSortOptionClick(sortOption) },
-                leadingIcon = {
-                  if (sortOption == currentSortOption) {
-                    Icon(
-                      imageVector = Icons.Default.Check,
-                      contentDescription = null,
-                      tint = MaterialTheme.colorScheme.primary,
-                    )
-                  }
-                },
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-              )
-            }
-          }
-        }
-
-        IconButton(onClick = onFavoritesClick) {
-          Image(
-            painter = painterResource(id = R.drawable.cool_shape_fav),
-            contentDescription = stringResource(R.string.content_desc_favorites),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-
-        IconButton(onClick = {
-          onViewTypeChange(
-            if (viewType == Constants.VIEW_TYPE_GRID) {
-              Constants.VIEW_TYPE_LIST
-            } else {
-              Constants.VIEW_TYPE_GRID
-            },
-          )
-        }) {
-          Image(
-            painter =
-              painterResource(
-                id =
-                  if (viewType == Constants.VIEW_TYPE_GRID) {
-                    R.drawable.cool_shape_list
-                  } else {
-                    R.drawable.cool_shape_grid
-                  },
-              ),
-            contentDescription = stringResource(R.string.content_desc_switch_view),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-
-        IconButton(onClick = onThemeChange) {
-          Image(
-            painter =
-              painterResource(
-                id =
-                  when (currentThemeMode) {
-                    ThemeMode.LIGHT -> R.drawable.cool_shape_night
-                    ThemeMode.DARK -> R.drawable.cool_shape_light
-                    ThemeMode.SYSTEM -> R.drawable.cool_shape_theme_system
-                  },
-              ),
-            contentDescription = stringResource(R.string.content_desc_toggle_theme),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-
-        IconButton(onClick = onFilterClick) {
-          Image(
-            painter = painterResource(id = R.drawable.cool_shape_filter),
-            contentDescription = stringResource(R.string.content_desc_filter),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-
-        IconButton(onClick = onCreateListClick) {
-          Image(
-            painter = painterResource(id = R.drawable.cool_shape_plus),
-            contentDescription = stringResource(R.string.content_desc_create_list),
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-      },
-      colors =
-        TopAppBarDefaults.topAppBarColors(
-          containerColor = MaterialTheme.colorScheme.surface,
-          titleContentColor = MaterialTheme.colorScheme.onSurface,
-          actionIconContentColor = MaterialTheme.colorScheme.onSurface,
-        ),
+    MainTopBar(
+      expandedDropdown = expandedDropdown,
+      onSortOptionClick = onSortOptionClick,
+      currentSortOption = currentSortOption,
+      onDropdownExpand = onDropdownExpand,
+      onSearchIconClick = onSearchIconClick,
+      onFavoritesClick = onFavoritesClick,
+      onViewTypeChange = onViewTypeChange,
+      viewType = viewType,
+      onThemeChange = onThemeChange,
+      currentThemeMode = currentThemeMode,
+      onFilterClick = onFilterClick,
+      onCreateListClick = onCreateListClick,
     )
   }
 }
+
+@Composable
+private fun MainTopBar(
+  expandedDropdown: Boolean,
+  onSortOptionClick: (SortOption) -> Unit,
+  currentSortOption: SortOption,
+  onDropdownExpand: () -> Unit,
+  onSearchIconClick: () -> Unit,
+  onFavoritesClick: () -> Unit,
+  onViewTypeChange: (String) -> Unit,
+  viewType: String,
+  onThemeChange: () -> Unit,
+  currentThemeMode: ThemeMode,
+  onFilterClick: () -> Unit,
+  onCreateListClick: () -> Unit,
+) {
+  TopAppBar(
+    title = { AppLogo() },
+    actions = {
+      SearchIcon(onClick = onSearchIconClick)
+      SortDropdown(
+        expanded = expandedDropdown,
+        onSortOptionClick = onSortOptionClick,
+        currentSortOption = currentSortOption,
+        onDropdownExpand = onDropdownExpand,
+      )
+      FavoritesIcon(onClick = onFavoritesClick)
+      ViewTypeIcon(
+        viewType = viewType,
+        onViewTypeChange = onViewTypeChange,
+      )
+      ThemeIcon(
+        currentThemeMode = currentThemeMode,
+        onThemeChange = onThemeChange,
+      )
+      FilterIcon(onClick = onFilterClick)
+      CreateListIcon(onClick = onCreateListClick)
+    },
+    colors = topAppBarColors(),
+  )
+}
+
+@Composable
+private fun AppLogo() {
+  Box(
+    modifier = Modifier.fillMaxWidth(),
+    contentAlignment = Alignment.Center,
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.cool_shape_arrow_right),
+      contentDescription = stringResource(R.string.app_name),
+      modifier =
+        Modifier
+          .size(Constants.ICON_SIZE_MEDIUM)
+          .align(Alignment.Center),
+    )
+  }
+}
+
+@Composable
+private fun SearchIcon(onClick: () -> Unit) {
+  IconButton(onClick = onClick) {
+    Image(
+      painter = painterResource(id = R.drawable.cool_shape_search),
+      contentDescription = stringResource(R.string.content_desc_search),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun SortDropdown(
+  expanded: Boolean,
+  onSortOptionClick: (SortOption) -> Unit,
+  currentSortOption: SortOption,
+  onDropdownExpand: () -> Unit,
+) {
+  Box {
+    IconButton(onClick = onDropdownExpand) {
+      Image(
+        painter = painterResource(id = R.drawable.cool_shape_sort),
+        contentDescription = stringResource(R.string.content_desc_sort),
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+      )
+    }
+    DropdownMenu(
+      expanded = expanded,
+      onDismissRequest = onDropdownExpand,
+      modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+    ) {
+      SortOption.values().forEach { sortOption ->
+        DropdownMenuItem(
+          text = {
+            Text(
+              text = stringResource(sortOption.stringRes),
+              style = MaterialTheme.typography.bodyMedium,
+              color = MaterialTheme.colorScheme.onSurface,
+            )
+          },
+          onClick = { onSortOptionClick(sortOption) },
+          leadingIcon = {
+            if (sortOption == currentSortOption) {
+              Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+              )
+            }
+          },
+          modifier =
+            Modifier.padding(
+              horizontal = Constants.PADDING_MEDIUM,
+              vertical = Constants.PADDING_SMALL,
+            ),
+        )
+      }
+    }
+  }
+}
+
+@Composable
+private fun FavoritesIcon(onClick: () -> Unit) {
+  IconButton(onClick = onClick) {
+    Image(
+      painter = painterResource(id = R.drawable.cool_shape_fav),
+      contentDescription = stringResource(R.string.content_desc_favorites),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun ViewTypeIcon(
+  viewType: String,
+  onViewTypeChange: (String) -> Unit,
+) {
+  IconButton(onClick = {
+    onViewTypeChange(
+      if (viewType == Constants.VIEW_TYPE_GRID) {
+        Constants.VIEW_TYPE_LIST
+      } else {
+        Constants.VIEW_TYPE_GRID
+      },
+    )
+  }) {
+    Image(
+      painter =
+        painterResource(
+          id =
+            if (viewType == Constants.VIEW_TYPE_GRID) {
+              R.drawable.cool_shape_list
+            } else {
+              R.drawable.cool_shape_grid
+            },
+        ),
+      contentDescription = stringResource(R.string.content_desc_switch_view),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun ThemeIcon(
+  currentThemeMode: ThemeMode,
+  onThemeChange: () -> Unit,
+) {
+  IconButton(onClick = onThemeChange) {
+    Image(
+      painter =
+        painterResource(
+          id =
+            when (currentThemeMode) {
+              ThemeMode.LIGHT -> R.drawable.cool_shape_night
+              ThemeMode.DARK -> R.drawable.cool_shape_light
+              ThemeMode.SYSTEM -> R.drawable.cool_shape_theme_system
+            },
+        ),
+      contentDescription = stringResource(R.string.content_desc_toggle_theme),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun FilterIcon(onClick: () -> Unit) {
+  IconButton(onClick = onClick) {
+    Image(
+      painter = painterResource(id = R.drawable.cool_shape_filter),
+      contentDescription = stringResource(R.string.content_desc_filter),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun CreateListIcon(onClick: () -> Unit) {
+  IconButton(onClick = onClick) {
+    Image(
+      painter = painterResource(id = R.drawable.cool_shape_plus),
+      contentDescription = stringResource(R.string.content_desc_create_list),
+      contentScale = ContentScale.Fit,
+      modifier = Modifier.size(Constants.ICON_SIZE_SMALL),
+    )
+  }
+}
+
+@Composable
+private fun topAppBarColors() =
+  TopAppBarDefaults.topAppBarColors(
+    containerColor = MaterialTheme.colorScheme.surface,
+    titleContentColor = MaterialTheme.colorScheme.onSurface,
+    actionIconContentColor = MaterialTheme.colorScheme.onSurface,
+  )
 
 @Composable
 private fun SearchTopBar(
